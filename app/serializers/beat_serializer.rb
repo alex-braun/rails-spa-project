293ec58e25a9cec94d922f8d3155f886077c6203
alex-braun <braunacb@gmail.com>
2016-08-18 11:43:38 -0400

    Added :order to serializer.

--- app/serializers/beat_serializer.rb
+++ app/serializers/beat_serializer.rb
@@ -1,4 +1,4 @@
 class BeatSerializer < ActiveModel::Serializer
-  attributes :id, :kick, :snare, :hatClose, :hatOpen, :clap
+  attributes :id, :kick, :snare, :hatClose, :hatOpen, :clap, :order
   belongs_to :pattern
 end
