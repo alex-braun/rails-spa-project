41aac4e981556dd787e940d880002a4e9f231c0e
alex-braun <braunacb@gmail.com>
2016-08-18 22:15:42 -0400

    changed :maps to :patterns.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   resources :users do
-    resources :maps
+    resources :patterns
   end
   resources :patterns do
     resources :beats
