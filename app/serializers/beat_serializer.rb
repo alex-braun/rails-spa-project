ec73b3434ae9f80ffb703702dc9b78d3cf0f3601
alex-braun <braunacb@gmail.com>
2016-08-18 22:05:38 -0400

    Added a has_one :pattern to beat serializer.
    many beats to a pattern.

--- app/serializers/beat_serializer.rb
+++ app/serializers/beat_serializer.rb
@@ -1,4 +1,4 @@
 class BeatSerializer < ActiveModel::Serializer
-  attributes :id, :kick, :snare, :hatClose, :hatOpen, :clap, :order
-  belongs_to :pattern
+  attributes :id, :kick, :snare, :hatClose, :hatOpen, :clap, :order, :pattern_id
+  has_one :pattern
 end
