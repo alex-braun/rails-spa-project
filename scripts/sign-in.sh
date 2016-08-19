c6b90d409c08bdae7f5530ef388c2a2672ef8bec
alex-braun <braunacb@gmail.com>
2016-08-18 22:19:52 -0400

    misc curl requests.

--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -8,3 +8,64 @@ curl --include --request POST http://localhost:3000/sign-in \
       "password": "an example password"
     }
   }'
+
+
+{"user":{"id":2,"email":"an@example.email","token":"BAhJIiVhODM0MmE5MWIwNzdhYjBlMGNhZWMzODMwZDMzY2QzYwY6BkVG--5ff75a92baa657fdbc6aea42216b
+
+curl --include --request DELETE http://localhost:3000/sign-out/2 \
+  --header "Authorization: Token token=BAhJIiVhODM0MmE5MWIwNzdhYjBlMGNhZWMzODMwZDMzY2QzYwY6BkVG--5ff75a92baa657fdbc6aea42216b"
+
+
+  curl --include --request POST http://localhost:3000/sign-in \
+    --header "Content-Type: application/json" \
+    --data '{
+      "credentials": {
+        "email": "an@example.email",
+        "password": "an example password"
+      }
+    }'
+
+curl --include --request POST http://localhost:3000/patterns \
+  --header "Authorization: Token token=BAhJIiU0OGQ0NjhjNjc3MGFlZmFkZDBkZjMzMDEwNTljYzZkMwY6BkVG--b801e93c3c272e6d54aaa1692583f4badeff9522" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "pattern": {
+      "name": "Folk",
+      "beats": "nothing"
+    }
+  }'
+
+
+
+
+
+BAhJIiVlZmU3NzU5ZWQ0MWIzZGU5MzJiYTUzODJmZTBkNzY4YwY6BkVG--1c4265b28218c9fa082014318057e795bcdecc12"
+
+curl --include --request POST "http://localhost:3000/patterns" \
+  --header "Authorization: Token token=BAhJIiVlZmU3NzU5ZWQ0MWIzZGU5MzJiYTUzODJmZTBkNzY4YwY6BkVG--1c4265b28218c9fa082014318057e795bcdecc12" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "pattern": {
+      "name": "Test #3"
+    }
+  }'
+  curl --include --request POST http://localhost:3000/patterns \
+  --header "Authorization: Token token=BAhJIiVlZmU3NzU5ZWQ0MWIzZGU5MzJiYTUzODJmZTBkNzY4YwY6BkVG--1c4265b28218c9fa082014318057e795bcdecc12" \
+  --header "Content-Type: application/json" \
+  --data $'{
+     "pattern": {
+       "name": "folk",
+    }
+  }'
+
+
+
+
+  curl --include --request POST http://localhost:3000/sign-in \
+    --header "Content-Type: application/json" \
+    --data '{
+      "credentials": {
+        "email": "1111",
+        "password": "7777"
+      }
+    }'
--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -4,9 +4,9 @@ curl --include --request POST http://localhost:3000/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "an@example.email",
-      "password": "an example password",
-      "password_confirmation": "an example password"
+      "email": "1111",
+      "password": "7777",
+      "password_confirmation": "7777"
     }
   }'
 
@@ -19,3 +19,22 @@ curl --include --request POST http://localhost:3000/sign-up \
       "password_confirmation": "an example password"
     }
   }'
+
+  curl --include --request POST http://localhost:3000/sign-up \
+    --header "Content-Type: application/json" \
+    --data '{
+      "credentials": {
+        "email": "7777",
+        "password": "7777",
+        "password_confirmation": "7777"
+      }
+    }'
+    curl --include --request POST http://localhost:3000/sign-up \
+      --header "Content-Type: application/json" \
+      --data '{
+        "credentials": {
+          "email": "2222",
+          "password": "7777",
+          "password_confirmation": "7777"
+        }
+      }'
